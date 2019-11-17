7289a99523f630e9a9e79bc510a4cebd5110dc25
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2019-11-17 21:53:12 +0300

    Update const.cpp

--- const.cpp
+++ const.cpp
@@ -1,12 +1,21 @@
-//find mistake in this constant code
+//which strings will cause error?
 
 const const struct Const {
   const const int const const f(const const int const const & const const Const) const const {
-    return const const (Const);
+    return (Const);
   }
 }const const;
 int main() {
-  const const Const const const a;
-  const const((const (a)).f(const const(3)));
-  return const(const(0));
+  const const Const const const a; //-1
+  const const Const const const * const const b = new (const const Const const const)(); //0
+  a.f(const int(3)); //1
+  b->f((const int(3))); //2
+  const (a.f((const int(3)))); //3
+  const int(a.f((const int(3)))); //4
+  const int(((const (a))).f((const int(3)))); //4
+  const int(((const a)).f((const int(3)))); //5
+  const int(((const Const(a))).f((const int(3)))); //6
+  const int (((const Const const (a))).f((const int (3)))); //7
+  const int(((const Const const (a))).f((const int(3)))); //8
+  return 0;
 }
