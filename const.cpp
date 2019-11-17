ab6374c5b74ad91601708d7945ada13c2a373271
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2019-11-17 21:26:36 +0300

    Create const.cpp

---
+++ const.cpp
@@ -0,0 +1,12 @@
+//find mistake in this constant code
+
+const const struct Const {
+  const const int const const f(const const int const const & const const Const) const const {
+    return const const (Const);
+  }
+}const const;
+int main() {
+  const const Const const const a;
+  const const((const (a)).f(const const(3)));
+  return const(const(0));
+}
